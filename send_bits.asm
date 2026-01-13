4b37a8ea23bdca570e9b23bc83669ff998fc8b7a
Paul-Fournet <144245169+Paul-Fournet@users.noreply.github.com>
2026-01-13 13:40:09 +0100

    Ajout fichier test boutons

---
+++ send_bits.asm
@@ -0,0 +1,12 @@
+
+
+MAIN_PROG CODE                      ; let linker place main program
+
+START
+
+    ; TODO Step #5 - Insert Your Program Here
+
+    MOVLW 0x55                      ; your instructions
+    GOTO $                          ; loop forever
+
+    END
